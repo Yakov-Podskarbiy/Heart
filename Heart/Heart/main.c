9ff9a149c5da6b86fabd6e31ee852d9e5dc1890c
Yakov-Podskarbiy <pjn92@mail.ru>
2017-08-25 16:40:49 +0700

    Create Project

---
+++ Heart/Heart/main.c
@@ -0,0 +1,18 @@
+/*
+ * Heart.c
+ *
+ * Created: 25.08.2017 16:36:16
+ * Author : BIOTOK_13
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
